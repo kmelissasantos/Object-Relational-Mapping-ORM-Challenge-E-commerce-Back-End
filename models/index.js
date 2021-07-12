5517b4a2fc0bc9e20a2e92d556db8690ad8e78ef
Melissa Santos <melissasantos@melissas-mbp.attlocal.net>
2021-07-11 23:20:43 -0400

    created associations

--- models/index.js
+++ models/index.js
@@ -5,12 +5,25 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'product_tag',
+  foreignKey
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'product_tag',
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
