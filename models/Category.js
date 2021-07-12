1c88c82ca02e53c0528b734b3cecf8baa2d2d549
Melissa Santos <melissasantos@melissas-mbp.attlocal.net>
2021-07-11 23:04:55 -0400

    created category model and defined all model columns

--- models/Category.js
+++ models/Category.js
@@ -4,9 +4,19 @@ const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
-Category.init(
-  {
+Category.init({
     // define columns
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+
+      category_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
   },
   {
     sequelize,
@@ -14,7 +24,6 @@ Category.init(
     freezeTableName: true,
     underscored: true,
     modelName: 'category',
-  }
-);
+  });
 
 module.exports = Category;
